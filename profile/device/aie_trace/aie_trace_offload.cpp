88e2be483556cd6d273197ab14cf953834496a27
Richard Byron <richard.byron@amd.com>
2026-04-21 10:58:12 -0700

    Free trace buffers

--- profile/device/aie_trace/aie_trace_offload.cpp
+++ profile/device/aie_trace/aie_trace_offload.cpp
@@ -40,7 +40,7 @@
 
 namespace xdp {
 
-
+  
 AIETraceOffload::AIETraceOffload
   ( void* handle, uint64_t id
   , PLDeviceIntf* dInt
@@ -487,7 +487,8 @@ void AIETraceOffload::startOffload()
 
 void AIETraceOffload::continuousOffload()
 {
-  if (!bufferInitialized && !initReadTrace()) {
+  if (!bufferInitialized) {
+    // initReadTrace failed to allocate the buffer, cannot offload
     offloadFinished();
     return;
   }
